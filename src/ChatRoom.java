ca4d5c6ae34e78b49486d39e0f55ce329ccab275
chinmay019 <chinmaynaik019@gmail.com>
2025-02-02 01:45:51 +0530

    58A Chat Room easier solution

--- src/ChatRoom.java
+++ src/ChatRoom.java
@@ -1,41 +1,21 @@
 package src;
 
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.stream.Collectors;
 
 public class ChatRoom {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        Stack<Character> stk = new Stack<>();
-
-        for(char ch : s.toCharArray()) {
-            if(stk.isEmpty() && ch == 'h') {
-                stk.push('h');
-            } else {
-                if(!stk.isEmpty()) {
-                    if (stk.size() == 1 && stk.peek() == 'h' && ch == 'e') {
-                        stk.push('e');
-                        continue;
-                    }
-                    if (stk.size() == 2 && stk.peek() == 'e' && ch == 'l') {
-                        stk.push('l');
-                        continue;
-                    }
-                    if (stk.size() == 3 && stk.peek() == 'l' && ch == 'l') {
-                        stk.push('l');
-                        continue;
-                    }
-                    if (stk.size() == 4 && stk.peek() == 'l' && ch == 'o') {
-                        stk.push('o');
-                    }
-                }
+        String target = "hello";
+        int k = 0, count = 0;
+        for(int i = 0; i < s.length(); i++ ) {
+            char ch = s.charAt(i);
+            if(count < 5 && ch == target.charAt(k)) {
+                k++;
+                count++;
             }
         }
-
-        String stkStr = stk.stream().map(a -> a.toString()).collect(Collectors.joining(""));
-        if(stkStr.equals("hello")) {
+        if(count == 5) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
