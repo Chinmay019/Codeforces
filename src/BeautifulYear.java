d0f2b89e3ca26d691264e68e8ed6c46671757ece
chinmay019 <chinmaynaik019@gmail.com>
2025-02-04 00:58:59 +0530

    271A Beautiful year solution simplified

--- src/BeautifulYear.java
+++ src/BeautifulYear.java
@@ -5,28 +5,18 @@ import java.util.Scanner;
 public class BeautifulYear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
+        int year = sc.nextInt();
 
         while(true) {
-            y++;
-            if(isDistinctDigits(y)) {
-                System.out.println(y);
+            year++;
+            int a = year/1000 % 10;
+            int b = year/100 % 10;
+            int c = year/10 % 10;
+            int d = year % 10;
+            if(a != b && a != c && a != d && b != c && b != d && c != d) {
+                System.out.println(year);
                 break;
             }
         }
-    }
-
-    public static boolean isDistinctDigits(int year) {
-        int[] digits = new int[10];
-        while (year > 0) {
-            int dig = year % 10;
-            digits[dig] += 1;
-            year /= 10;
-        }
-        for(int i = 0; i < 10; i++ ) {
-            if(digits[i] >= 2)
-                return false;
-        }
-        return true;
     }
 }
