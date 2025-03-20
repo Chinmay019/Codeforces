bf9091f429a6b171beb6f03bc04a4c058f2ba6cf
chinmay019 <chinmaynaik019@gmail.com>
2025-03-20 12:01:23 +0530

    Codeforces solution for problem 1352A : Sum of Round Numbers

--- SumOfRoundNumbers1352A.java
+++ SumOfRoundNumbers1352A.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class SumOfRoundNumbers1352A {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[5];
+            int i = 0, count = 0;
+            while(n > 0) {
+                arr[i] = n % 10;
+                if(arr[i] != 0)
+                    count++;
+                i++;
+                n /= 10;
+            }
+            System.out.println(count);
+            for(int j = 0; j < 5; j++) {
+                int num = (int) Math.pow(10, j) * arr[j];
+                if(num != 0)
+                    System.out.print(num +" ");
+            }
+            System.out.println();
+        }
     }
 }
