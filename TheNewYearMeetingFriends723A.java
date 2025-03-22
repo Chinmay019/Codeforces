40f134a1a14ec2d1c8a10b0a3c7450bcf41d4b14
chinmay019 <chinmaynaik019@gmail.com>
2025-03-23 02:48:49 +0530

    Codeforces solution 723A The New Year Meeting Fiends JAVA solution

---
+++ TheNewYearMeetingFriends723A.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TheNewYearMeetingFriends723A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int[] arr = {n1, n2, n3};
+        Arrays.sort(arr);
+        int ans = (arr[1] - arr[0]) + (arr[2] - arr[1]);
+        System.out.println(ans);
+    }
+}
