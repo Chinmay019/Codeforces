56e77549b6750550b132e374f002102e908a24e9
chinmay019 <chinmaynaik019@gmail.com>
2025-09-18 19:36:07 +0530

    Codeforces solution for 9A Die Roll JAVA solution

---
+++ DieRoll9A.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DieRoll9A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int d = Math.max(y , w);
+        switch (d) {
+            case 6:
+                System.out.println("1/6");
+                break;
+            case 5:
+                System.out.println("1/3");
+                break;
+            case 4:
+                System.out.println("1/2");
+                break;
+            case 3:
+                System.out.println("2/3");
+                break;
+            case 2:
+                System.out.println("5/6");
+                break;
+            case 1:
+                System.out.println("1/1");
+        }
+    }
+}
