ffd5d46a226648ca6ebca1ff6da0487f1962ab2e
chinmay019 <chinmaynaik019@gmail.com>
2026-02-01 15:54:19 +0530

    Codeforces solution for Problem Halloumi Boxes 1903A in JAVA

---
+++ HalloumiBoxes1903A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HalloumiBoxes1903A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            boolean isNonDec = true;
+            for(int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+                if(i > 0 && arr[i] < arr[i - 1])
+                    isNonDec = false;
+            }
+            System.out.println(k >= 2 ? "Yes" : isNonDec ? "Yes" : "no");
+        }
+    }
+}
