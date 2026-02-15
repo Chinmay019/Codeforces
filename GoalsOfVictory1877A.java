f37fa6c7e03205990d4be32ce4ba3034ddaf256e
chinmay019 <chinmaynaik019@gmail.com>
2026-02-15 15:42:07 +0530

    Codeforces solution for Goals Of Victory 1877A in JAVA

---
+++ GoalsOfVictory1877A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GoalsOfVictory1877A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int sum = 0;
+            for (int j = 0; j < n - 1; j++) {
+                sum += sc.nextInt();
+            }
+            System.out.println(-sum);
+        }
+    }
+}
